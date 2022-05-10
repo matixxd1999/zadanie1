7c3388df51148b82efb3e15c1499ee7a15a1294f
Mateusz Labud <mateuszwwe999@interia.pl>
2022-05-10 13:23:18 +0200

    Przy stworzeniu nowego hasła trzeba wpisać je dwa razy

--- src/Form/ChangePasswordType.php
+++ src/Form/ChangePasswordType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Admin;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,15 +15,21 @@ class ChangePasswordType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('password', PasswordType::class, ['label' => 'nowe hasło'])
-            ->add('Zatwierdź', SubmitType::class)
+        ->add('password', RepeatedType::class, [
+            'type' => PasswordType::class,
+            'invalid_message' => 'Podane hasła nie są takie same',
+            'options' => ['attr' => ['class' => 'password-field']],
+            'required' => true,
+            'first_options'  => ['label' => 'Nowe Hasło'],
+            'second_options' => ['label' => 'Powtórz Hasło']])
+        ->add('Zatwierdź', SubmitType::class)
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Admin::class
+            'password' => Admin::class
         ]);
     }
 }
